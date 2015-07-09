b107f1a4f6c55ce3d99d4a3a53dca5b7da29cfc2
cctv1237 <marklf@163.com>
2015-07-09 13:41:02 +0800

    Initial Commit

---
+++ ImageUploadDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageUploadDemo
+//
+//  Created by LF on 15/7/9.
+//  Copyright (c) 2015年 Beauty Sight Network Technology Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
